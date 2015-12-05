5773e19645224eacdc20990bbe4f7eaf609ecdda
Lisa Zhang <amphithere.19@gmail.com>
2015-12-05 11:20:11 -0700

    Input now working, next is databases

--- app/src/main/java/com/example/lisazhang/mypetchickens/MainActivity.java
+++ app/src/main/java/com/example/lisazhang/mypetchickens/MainActivity.java
@@ -1,18 +1,38 @@
 package com.example.lisazhang.mypetchickens;
 
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private String name;
+    private EditText nameInput;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nameInput = (EditText) findViewById(R.id.nameText);
+        nameInput.setOnEditorActionListener( new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                boolean handled = false;
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    name = nameInput.getText().toString();
+                }
+                return handled;
+            }
+            });
     }
 
     @Override
@@ -38,8 +58,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void buttonOnClick(View v){
-        Button button = (Button) v;
-        button.setText("DOODOO");
+        button = (Button) v;
+        button.setText(name);
 
     }
+
+
 }
